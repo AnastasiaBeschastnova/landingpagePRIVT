2bec3471036074a566659b81cb041621fd59b469
David <49199808+SivkoDavid@users.noreply.github.com>
2020-05-29 19:00:07 +0400

    настроенный

--- js/custom.js
+++ js/custom.js
@@ -1,81 +1,81 @@
 // JavaScript Document
-(function(){
-  "use strict";
-//Banner height
-$(document).ready(function() {
+(function() {
+    "use strict";
+    //Banner height
+    $(document).ready(function() {
 
-	var viewport_width = window.innerWidth;
-	var viewport_height = window.innerHeight;
+        var viewport_width = window.innerWidth;
+        var viewport_height = window.innerHeight;
 
-//Banner
-	$('.banner,.carousel .item').height((viewport_height));
-	$(window).resize(function() {
-	  var viewport_width = window.innerWidth;
-	  var viewport_height = window.innerHeight;
-		$('.banner,.carousel .item').height((viewport_height));
-	});
-	});
-	
-//Page Smooth Scrolling
+        //Banner
+        $('.banner,.carousel .item').height((viewport_height));
+        $(window).resize(function() {
+            var viewport_width = window.innerWidth;
+            var viewport_height = window.innerHeight;
+            $('.banner,.carousel .item').height((viewport_height));
+        });
+    });
 
-$(document).ready(function() {
-		$('ul.navi a').bind('click',function(event){
-			var $anchor = $(this);
-	
-			$('html, body').stop().animate({
-			scrollTop:( $($anchor.attr('href')).offset().top-0) 
-			}, 1500,'easeInOutExpo');
-                    /*
-                    if you don't want to use the easing effects:
-                    $('html, body').stop().animate({
-                        scrollTop: $($anchor.attr('href')).offset().top
-                    }, 1000);
-                    */
-					event.preventDefault();
-					});
-			});
+    //Page Smooth Scrolling
 
-//Opt-in Form
+    $(document).ready(function() {
+        $('ul.navi a').bind('click', function(event) {
+            var $anchor = $(this);
 
-$(document).ready(function(){
- 
-$('#submit').click(function(){
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset().top - 0)
+            }, 1500, 'easeInOutExpo');
+            /*
+            if you don't want to use the easing effects:
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1000);
+            */
+            event.preventDefault();
+        });
+    });
 
-$.post("send.php", $("#contactform").serialize(),  function(response) {
-$('#message').html(response);
-$( "#message" ).show(1000);
-});
-return false;
- 
-});	
-});
+    //Opt-in Form
 
-//Contact Form
+    $(document).ready(function() {
 
-$(document).ready(function(){
- 
-$('#sendmsg').click(function(){
+        $('#submit').click(function() {
 
-$.post("contact.php", $("#contactmsg").serialize(),  function(response) {
-$('#contact-message').html(response);
-$( "#contact-message" ).show(1000);
-});
-return false;
- 
-});	
-});			
-//Flexslider for testimonials
-	// Can also be used with $(document).ready()
-	$(window).load(function() {
-	 
-	  $('#testimonial').flexslider({
-		animation: "slide",
-		controlNav: true,
-		directionNav:false,
-		animationLoop: true,
-		slideshow: false,
-	
-	  });
-	});
+            $.post("send.php", $("#contactform").serialize(), function(response) {
+                $('#message').html(response);
+                $("#message").show(1000);
+            });
+            return false;
 
-		})();
+        });
+    });
+
+    //Contact Form
+
+    $(document).ready(function() {
+
+        $('#sendmsg').click(function() {
+
+            $.post("contact.php", $("#contactmsg").serialize(), function(response) {
+                $('#contact-message').html(response);
+                $("#contact-message").show(1000);
+            });
+            return false;
+
+        });
+    });
+    //Flexslider for testimonials
+    // Can also be used with $(document).ready()
+    $(document).ready(function() {
+
+        $('#testimonial').flexslider({
+            animation: "slide",
+            controlNav: true,
+            directionNav: false,
+            animationLoop: true,
+            slideshow: false,
+
+        });
+    });
+
+})();
